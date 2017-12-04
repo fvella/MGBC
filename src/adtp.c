23b9d7e78a3cabd35c1ed5d5bdd591e8a2c549d3
Flavio Vella <flavio@dividiti.com>
2017-12-04 16:13:30 +0000

    Fix code formatting

--- src/adtp.c
+++ src/adtp.c
@@ -3,10 +3,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-static int stringCmp( const void *a, const void *b)
+static int stringCmp(const void *a, const void *b)
 {
-     return strcmp(a,b);
-
+    return strcmp(a, b);
 }
 
 /*
@@ -14,65 +13,69 @@ static int stringCmp( const void *a, const void *b)
   and assigns a local rank that can be used to map a process to a device.
   This function needs to be called by all the MPI processes.
 */
-int  assignDeviceToProcess()
+int assignDeviceToProcess()
 {
 #ifdef MPI
-      char     host_name[MPI_MAX_PROCESSOR_NAME];
-      char (*host_names)[MPI_MAX_PROCESSOR_NAME];
-      MPI_Comm nodeComm;
+    char host_name[MPI_MAX_PROCESSOR_NAME];
+    char(*host_names)[MPI_MAX_PROCESSOR_NAME];
+    MPI_Comm nodeComm;
 
 #else
-      char     host_name[20];
+    char host_name[20];
 #endif
-      int myrank;
-      int gpu_per_node;
-      int n, namelen, color, rank, nprocs;
-      size_t bytes;
-/*
-      if (chkseGPU()<1 && 0) {
-        fprintf(stderr, "Invalid GPU Serial number\n");
-	exit(EXIT_FAILURE);
-      }
-*/
+    int myrank;
+    int gpu_per_node;
+    int n, namelen, color, rank, nprocs;
+    size_t bytes;
+    /*
+          if (chkseGPU()<1 && 0) {
+            fprintf(stderr, "Invalid GPU Serial number\n");
+        exit(EXIT_FAILURE);
+          }
+    */
 
 #ifdef MPI
-      MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-      MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
-      MPI_Get_processor_name(host_name,&namelen);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+    MPI_Get_processor_name(host_name, &namelen);
 
-      bytes = nprocs * sizeof(char[MPI_MAX_PROCESSOR_NAME]);
-      host_names = (char (*)[MPI_MAX_PROCESSOR_NAME]) malloc(bytes);
+    bytes = nprocs * sizeof(char[MPI_MAX_PROCESSOR_NAME]);
+    host_names = (char(*)[MPI_MAX_PROCESSOR_NAME])malloc(bytes);
 
-      strcpy(host_names[rank], host_name);
+    strcpy(host_names[rank], host_name);
 
-      for (n=0; n<nprocs; n++)
-      {
-       MPI_Bcast(&(host_names[n]),MPI_MAX_PROCESSOR_NAME, MPI_CHAR,n, MPI_COMM_WORLD);
-      }
+    for (n = 0; n < nprocs; n++)
+    {
+        MPI_Bcast(&(host_names[n]), MPI_MAX_PROCESSOR_NAME, MPI_CHAR, n,
+                  MPI_COMM_WORLD);
+    }
 
+    qsort(host_names, nprocs, sizeof(char[MPI_MAX_PROCESSOR_NAME]), stringCmp);
 
-      qsort(host_names, nprocs, sizeof(char[MPI_MAX_PROCESSOR_NAME]), stringCmp);
+    color = 0;
 
-      color = 0;
+    for (n = 0; n < nprocs; n++)
+    {
+        if (n > 0 && strcmp(host_names[n - 1], host_names[n]))
+            color++;
+        if (strcmp(host_name, host_names[n]) == 0)
+            break;
+    }
 
-      for (n=0; n<nprocs; n++)
-      {
-        if(n>0&&strcmp(host_names[n-1], host_names[n])) color++;
-        if(strcmp(host_name, host_names[n]) == 0) break;
-      }
+    MPI_Comm_split(MPI_COMM_WORLD, color, 0, &nodeComm);
 
-      MPI_Comm_split(MPI_COMM_WORLD, color, 0, &nodeComm);
-
-      MPI_Comm_rank(nodeComm, &myrank);
-      MPI_Comm_size(nodeComm, &gpu_per_node);
+    MPI_Comm_rank(nodeComm, &myrank);
+    MPI_Comm_size(nodeComm, &gpu_per_node);
 
 #else
-     //*myrank = 0;
-     return 0;
+    //*myrank = 0;
+    return 0;
 #endif
 
-//      printf ("Assigning device %d  to process on node %s rank %d\n",*myrank,  host_name, rank );
-      /* Assign device to MPI process, initialize BLAS and probe device properties */
-      //cudaSetDevice(*myrank);
-      return myrank;
+    //      printf ("Assigning device %d  to process on node %s rank
+    //      %d\n",*myrank,  host_name, rank );
+    /* Assign device to MPI process, initialize BLAS and probe device properties
+     */
+    // cudaSetDevice(*myrank);
+    return myrank;
 }
